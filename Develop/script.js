a5e4c5a5c3fbef823ede9d8743be5b85384dc841
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-03-23 21:09:19 -0500

    began creating variables for characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// added variable arrays for user prompts
+
+var passwordLength = parseInt()
+var numeric = ["0","1","2","3","4","5","6","7","8","9"]
+var specialCharacters = []
+var uppercaseCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
 function generatePassword() {
-  return "password";
-}
-
-function indexRandomizer() {
-
+  // return generated password
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
@@ -23,6 +25,8 @@ generateBtn.addEventListener("click", writePassword);
 
 if (passwordLength < 8) {
   alert("Please enter a password that's at least 8 chracters.")
+}else if (passwordLength > 128) {
+  alert("Please enter a password that's fewer than 128 characters.")
 }
 
 // 1) create variables to save user input from prompts
@@ -32,14 +36,14 @@ if (passwordLength < 8) {
 //    -uppercaseCharacters (boolean)
 //    -lowercaseCharacters (boolean)
 //    -alert, confirm, prompt
-//    --use conditionals to validate inputs (min characters, max characters, no inputs)
+//    --use conditionals to validate inputs (min characters 8, max characters 128, no inputs)
 //    --"if array is true, push at least one character, if false, don't push character"
 //    --start with one variable, copy paste format for rest
 
 // 2) create an arrays for each type of password character
 //    -specialCharacters, uppercase, lowercase, numeric
 
-// 3) use randomizer function to select characters from each array
+// 3) use randomizer function to select characters from each array 
 
 // 4) generatePassword function - takes user input, grab characters from array, generate password
 
